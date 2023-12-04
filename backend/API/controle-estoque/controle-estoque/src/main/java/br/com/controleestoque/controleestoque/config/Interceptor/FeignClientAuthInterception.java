3c828e0b35cd3bd5f21fd16c98e2775c70f136e2
RobertoAntunes <roh.antunes35@gmail.com>
2023-12-04 16:05:34 -0300

    a

--- backend/API/controle-estoque/controle-estoque/src/main/java/br/com/controleestoque/controleestoque/config/Interceptor/FeignClientAuthInterception.java
+++ backend/API/controle-estoque/controle-estoque/src/main/java/br/com/controleestoque/controleestoque/config/Interceptor/FeignClientAuthInterception.java
@@ -1,11 +1,7 @@
 package br.com.controleestoque.controleestoque.config.Interceptor;
 
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+ 
 
-import jakarta.servlet.http.HttpServletRequest;
-
-
-public class FeignClientAuthInterception implements RequestInterceptor{
+public class FeignClientAuthInterception{
 
 }
